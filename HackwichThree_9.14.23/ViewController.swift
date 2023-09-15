5394b5e5b06fd5a5844ad31fff81f4b64ba1d4f4
Lorisa Bulosan <lorisab@hawaii.edu>
2023-09-14 14:27:36 -1000

    Completed Parts 1-6

--- HackwichThree_9.14.23/ViewController.swift
+++ HackwichThree_9.14.23/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+//please declare your global variables here for part 3
 
-    override func viewDidLoad() {
+    var firstString = "The background color will turn blue"
+    var secondString = "The background color will turn green"
+    var thirdString = "cool"
+    
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    {
+        if firstString == "The background color will turn green"
+        {
+            self.view.backgroundColor = UIColor.red
+        }
+        
+        else
+        {
+            self.view.backgroundColor = UIColor.blue
+            firstLabel.text = "This is \(thirdString)"
+        }
+        
+      
+        
+    }
+    
 }
 
