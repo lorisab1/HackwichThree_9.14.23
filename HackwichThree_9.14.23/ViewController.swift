80827b6e810c7ac2e194a2c2c8783ee9593e80eb
Lorisa Bulosan <lorisab@hawaii.edu>
2023-09-16 23:32:34 -1000

    Completed Problem Sets

--- HackwichThree_9.14.23/ViewController.swift
+++ HackwichThree_9.14.23/ViewController.swift
@@ -8,21 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-//please declare your global variables here for part 3
-
+    //please declare your global variables here for part 3
+    
     var firstString = "The background color will turn blue"
     var secondString = "The background color will turn green"
     var thirdString = "cool"
     
+    //Part 7 Constant Variables
+    
+    let lemon = 1
+    let lime = 2
+    
+    //Part 5
     
     @IBOutlet weak var firstLabel: UILabel!
+    
+    //Part 7
+    
+   
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    
+    @IBOutlet weak var thirdLabel: UILabel!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func changeColorButtonPressed(_ sender: Any)
     {
         if firstString == "The background color will turn green"
@@ -36,9 +51,40 @@ class ViewController: UIViewController {
             self.firstLabel.text = "This is \(thirdString)"
         }
         
-      
         
     }
     
-}
+    //Part 7 Problem Set #1
+    @IBAction func changeTextButtonPressed(_ sender: Any)
+    {
+        if thirdString == "cool"
+        {
+            self.secondLabel.text = "Hello"
+        }
+        else
+        {
+            self.thirdLabel.text = "Goodbye"
+        }
+        
+    }
+    
+    //Part 7 Problem Set #2
+    
 
+    @IBAction func changeBackgroundButtonPressed(_ sender: Any)
+    {
+        if lemon > lime
+        {
+            print("The code in this block will not execute")
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.green
+            let orange = lemon + lime
+            print(orange)
+        }
+        
+    }
+    
+    
+}
